e6be43bc4a9a209bed10b94bfaad2afe06e48eaa
Marc Poppy <poppy.marc@gmail.com>
2020-01-25 19:42:36 +0000

    Automatically backed up by Learn

--- db/migrate/006_add_catchphrase_to_characters.rb
+++ db/migrate/006_add_catchphrase_to_characters.rb
@@ -1,7 +1,7 @@
 
 class AddCatchPhraseToCharacters < ActiveRecord::Migration[5.2]
   def change
-    add_column :characters, :catchphrase :string
+    add_column :characters, :catchphrase, :string
 
   end
 end
