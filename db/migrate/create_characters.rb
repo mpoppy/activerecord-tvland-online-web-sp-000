93b9bfd17bebd158488ed1766e58ca6f5956ef84
Marc Poppy <poppy.marc@gmail.com>
2020-01-25 19:38:28 +0000

    Automatically backed up by Learn

--- db/migrate/create_characters.rb
+++ db/migrate/create_characters.rb
@@ -2,6 +2,9 @@
 class CreateCharacters < ActiveRecord::Migrations[5.1]
   def change
     create_table :characters do |t|
+      t.string :name
+      t.integer :actor_id
+      t.integer :show_id
     end
   end
 end
